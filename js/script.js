05670111799cac72711308f1fd0c105d329b85d5
stevandjokic94 <stevandjokic94@live.com>
2022-01-04 15:57:33 +0100

    added slider on index of CEOs, about added slider, puth norway flag

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ function openNav(button){
 	// button.classList.toggle('opened');
 	// button.setAttribute('aria-expanded', button.classList.contains('opened'));
 	$('#myNav').css('width', "100%");
+	$('body').addClass('overlayBody');
 }
 /* Open when someone clicks on the span element */
 
@@ -23,10 +24,14 @@ function openNav(button){
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   $('#myNav').css('width', "0%");
+	$('body').removeClass('overlayBody');
 }
 
 $(window).resize(() => {
 	let presentation = $('#presentation');
+	console.log(window.screen.width);
+	presentation[0].height = window.screen.width * .9;
+	presentation[0].width = window.screen.width * .9;
 	console.log(presentation[0].height);
 	console.log(presentation[0].width);
 });
